be39203140f338bfea6fd496ffdc22b623d8a602
lmosine <wangsh114116@163.com>
2020-12-02 01:12:43 +0800

    完成项目基础目录结构

---
+++ src/pages/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
